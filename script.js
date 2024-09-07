e240c2a8627ff5d8713d3be0e047941c2e9da5e4
gnandal <egnandal500@yahoo.com>
2024-09-07 19:58:48 +0100

    Challenge code end

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ const socialMediaComponent = document.querySelector(".social-media");
 const authorComponent = document.querySelector(".author");
 let classAdded = false;
 
-const toggleShareOptions = () => {
+const toggleShareOptions = (event) => {
 
     if(window.innerWidth > 950) {
         shareOptionComponent.style.width = "fit-content";
@@ -23,6 +23,8 @@ const toggleShareOptions = () => {
     }
 
     socialMediaComponent.style.display = classAdded ? "flex" : "none";
+    event.style.backgroundColor = classAdded ? "hsl(214, 17%, 51%)" : "hsl(210, 46%, 95%)";
+    event.childNodes[0].style.filter = classAdded ? "brightness(0) saturate(100%) invert(100%) sepia(2%) saturate(4836%) hue-rotate(180deg) brightness(105%) contrast(108%)" : "hue-rotate(0deg)";
 }
 
 onresize = (event) => {
@@ -37,7 +39,6 @@ onresize = (event) => {
         authorComponent.style.display = classAdded ? "none" : "flex"; 
         
         shareOptionComponent.style.width = classAdded ? "100%" : "20%"
-        shareOptionComponent.style.backgroundColor = classAdded ? "hsl(214, 17%, 51%)" : "#FFF"
+        shareOptionComponent.style.backgroundColor = classAdded ? "hsl(214, 17%, 51%)" : " hsl(210, 46%, 95%)";
     }
-    
 };
